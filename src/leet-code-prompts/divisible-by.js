1eb96c13ed096719f23c02665b1eba28225c4e94
Ann Hochworter <ann.hochworter@gmail.com>
2024-03-01 12:14:03 -0700

    add solution to isDivisible challenge

--- src/leet-code-prompts/divisible-by.js
+++ src/leet-code-prompts/divisible-by.js
@@ -9,12 +9,17 @@ Output 2 --> [2, 4, 6]
 Pseudocode
 GOAL -> determine if the numbers in an array are divisible by the given divisor
 INPUT -> an array of numbers and the divisor
+OUTPUT -> an array of numbers, divisible by the divisor
 HOW ->  iterate through the array and check each element for a remainder based on the divisor
+METHOD -> filter (based on if there's a remainder)
 
 */
 
 const divisibleBy = (numbers, divisor) => {
-  console.log('Divisible By');
+  const isDivisible = numbers.filter(number => {
+    return number % divisor === 0;
+  });
+  return isDivisible;
 };
 
 export default divisibleBy;
--- test/leet-code-tests/divisible-by-test.js
+++ test/leet-code-tests/divisible-by-test.js
@@ -5,4 +5,13 @@ describe('divisibleBy', () => {
   it('should be a function', () => {
     expect(divisibleBy).to.be.a('function');
   });
+  it('should return an array of numbers that are divisible by the given divisor', () => {
+    let numbers = [1, 2, 3, 4, 5, 6];
+    let divisor = 2;
+    // console.log(numbers);
+    // console.log(divisor);
+    const results = divisibleBy(numbers, divisor);
+
+    expect(results).to.deep.equal([2, 4, 6]);
+  });
 });
