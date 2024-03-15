a81353ef3f0fd7281cdac67b65de35d0f1abce94
Ann Hochworter <ann.hochworter@gmail.com>
2024-03-15 14:40:45 -0600

    Write the function to pass the provided unit tests

--- src/turing-daily-pseudocode/phone-pseudocode.js
+++ src/turing-daily-pseudocode/phone-pseudocode.js
@@ -1,7 +1,21 @@
 // Practice planning out how you would approach your code to pass the tests listed in the tests.js file. Your planning process should include thorough pseudocode.
 
 function createPhone(phoneNumber, contacts) {
-  return {};
+  console.log('phoneNumber:', phoneNumber);
+  console.log('contacts:', contacts);
+  let phone = {
+    number: phoneNumber,
+    contacts: contacts,
+    call: function (contactName) {
+      for (let i = 0; i <= contacts.length; i++) {
+        if (contactName === contacts[i]) {
+          return `Calling ${contactName}...`;
+        }
+      }
+      return `Unknown User: ${contactName}`;
+    },
+  };
+  return phone;
 }
 
 export default createPhone;
